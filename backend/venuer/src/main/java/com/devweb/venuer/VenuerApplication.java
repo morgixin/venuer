4ba7caa292f0453532dd882f9ac9c7e3c3af2c4e
Ana Ferreira <anabsf@id.uff.br>
2025-01-11 15:21:36 -0300

    add spring boot setup and deps

---
+++ backend/venuer/src/main/java/com/devweb/venuer/VenuerApplication.java
@@ -0,0 +1,13 @@
+package com.devweb.venuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenuerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenuerApplication.class, args);
+	}
+
+}
